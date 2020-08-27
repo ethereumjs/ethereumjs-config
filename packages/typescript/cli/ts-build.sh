6afd9e3f01f75d233ee7d66611ab8141c523a46b
Everton Fraga <ev@ethereum.org>
2020-08-26 23:29:17 -0400

    typescript: fixes browser builds; makes browser builds optional

--- packages/typescript/cli/ts-build.sh
+++ packages/typescript/cli/ts-build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -e
 set -o xtrace
-tsc -p ./tsconfig.prod.json && test -f ./tsconfig.browser.json && tsc -p ./tsconfig.browser.json
+
+tsc -p ./tsconfig.prod.json
+
+if [ -f ./tsconfig.browser.json ];
+then
+    tsc -p ./tsconfig.browser.json
+else
+    echo "Skipping browser build, because no tsconfig.browser.json file is present."
+fi
